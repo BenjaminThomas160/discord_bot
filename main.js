82546748fe99b1233a7dce6b06507c6afd1d7684
Ben <benjamin.thomas160@gmail.com>
2021-12-12 11:22:10 +1100

    changed way of parsing commands, added custom prefix

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 var CONFIG = require('./config.json');
 const Discord = require('discord.js');
+var botconfig = require('./botconfig.json');
 const client = new Discord.Client({
 	allowedMentions: {
 		parse: ['users', 'roles'],
@@ -28,33 +29,46 @@ function send_qs(channel) {
 
 var timer;
 client.on('messageCreate', async message => {
-	if (message.content === "!set") {
+	if (message.channel.type === "dm") return;
+	if (message.author.bot) return;
+
+	let prefix = botconfig.prefix;
+
+	if (!message.content.startsWith(prefix)) return;
+	let args = message.content.slice(prefix.length).trim().split(/ +/g);
+	let cmd = args.shift().toLowerCase();
+	console.log(cmd);
+	
+	if (cmd === "prefix_set") {
+		botconfig.prefix = args[0];
+	}
+	if (cmd  === "set") {
 		channel = message.channel;
 		timer = setInterval(send_qs, 5000, channel);
 	}
-	if (message.content === "!stop") {
+	if (cmd  === "stop") {
 		clearInterval(timer);
 	}
-
 	// bee movie quote
-	if (message.content === "!b") {
+	if (cmd === "b") {
 		var quotes = CONFIG.bquotes;
 		num = Math.floor(Math.random() * quotes.length);
 		message.channel.send(quotes[num]);	
 	}
 	// add a quote
-	if (message.content.split(" ")[0] === "!addq") {
+	if (cmd === "q_add") {
 		var quotes = CONFIG.uQuotes;
 		new_quote = message.content.substring(message.content.indexOf(" ") + 1);
 		console.log(new_quote);
 		if (quotes.includes(new_quote)) {
-			message.channel.send("quote already exists use !f_addq to force add");	
+			message.channel.send("quote already exists use !fq_add to force add");	
 		} else if (message.content.split(" ")[0] != undefined) {
 			quotes.push(new_quote);
+			message.channel.send("added quote");
 		}
 	}
 	// force add a quote
-	if (message.content.split(" ")[0] === "!f_addq") {
+	if (cmd === "fq_add") {
 		var quotes = CONFIG.uQuotes;
 		new_quote = message.content.substring(message.content.indexOf(" "));
 		console.log(new_quote);
@@ -63,55 +77,59 @@ client.on('messageCreate', async message => {
 		}
 	}
 	// send user quote
-	if (message.content === "!quote") {
+	if (cmd === "quote") {
 		var quotes = CONFIG.uQuotes;
 		if (quotes.length == 0) {
-			message.channel.send("There are no quotes, use !addq to add a quote");
+			message.channel.send("There are no quotes, use !q_add to add a quote");
 		} else {
 			num = Math.floor(Math.random() * quotes.length);
 			message.channel.send(quotes[num]);
 		}
 	}
 	// quote list
-	if (message.content.split(" ")[0] === "!q_list") {
+	if (cmd === "q_list") {
 		var str = message.content.split(" ");
 		var quotes = CONFIG.uQuotes;
 		var end = 50;
 		var start = 0;
 		var out = "";
-		if (str[1] === '-a') {
-			end = quotes.length;
-			start = 0;
-		}
-		if (str[2] != undefined) {
-			if (!isNaN(str[1]) && !isNaN(str[2])) {
-				start = parseInt(str[1]);
-				end = parseInt(str[2]);
+		if (quotes.length > 0) {
+			if (str[1] === '-a') {
+				end = quotes.length;
+				start = 0;
 			}
-		} else if (str[1] != undefined) {
-			if (!isNaN(str[1])) {
-				end = parseInt(str[1]);
+			if (str[2] != undefined) {
+				if (!isNaN(str[1]) && !isNaN(str[2])) {
+					start = parseInt(str[1]);
+					end = parseInt(str[2]);
+				}
+			} else if (str[1] != undefined) {
+				if (!isNaN(str[1])) {
+					end = parseInt(str[1]);
+				}
 			}
-		}
-		for (let i = start; i < quotes.length; i++) {
-			if (i > end) {
-				break;
+			for (let i = start; i < quotes.length; i++) {
+				if (i > end) {
+					break;
+				}
+				out += i + ": " + quotes[i] + "\n";	
 			}
-			out += i + ": " + quotes[i] + "\n";	
-		}
-		message.channel.send(out);
+			message.channel.send(out);
 
-	}
+			} else {
+				message.channel.send("There are no quotes, add quotes with !")
+			}
+		}
 	// hello world
-	if (message.content === "!hello") {
+	if (cmd === "hello") {
 		message.channel.send("hello world");
 	}
 	// E
-	if (message.content === "!E") {
+	if (cmd === "E") {
 		message.channel.send("E");
 	}
 	// sussy
-	if (message.content === "!sus") {
+	if (cmd === "sus") {
 		message.channel.send(
 			`
 			⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⣤⣤⣤⣤⣤⣶⣦⣤⣄⡀⠀⠀⠀⠀⠀⠀⠀⠀
@@ -136,7 +154,7 @@ client.on('messageCreate', async message => {
 			`);
 	}
 	// sussy wussy
-	if (message.content ===  "!;)") {
+	if (cmd ===  ";)") {
 		num = Math.floor(Math.random() * 100);
 		name = message.member.user.toString();
 		if (name === '<@413626625457127424>') {
@@ -147,7 +165,6 @@ client.on('messageCreate', async message => {
 	}
 })
 
-var TOKEN = require('./tokens.json');
-
+var TOKEN = require('./botconfig.json');
 
 client.login(TOKEN.token);
